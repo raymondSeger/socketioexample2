e793658d21ba489268593f031a3ee2af38c1ce44
Lukman <lukman1291@live.com>
2014-12-03 09:18:30 +0700

    Save point

--- chat.js
+++ chat.js
@@ -2,11 +2,7 @@
 // Default Modules from nodeJS, etc
 var http  = require('http');
 
-// My variables
-var billyJSONObject = {'name': 'billy'};
-var usernames = {};
-var numUsers = 0;
-
+//this is connection server listen on port
 var server = http.createServer(function(request, response){
   console.log('Server NodeJS created.');
 }).listen(9900, function(){
@@ -21,24 +17,11 @@ io.on('connection', function(socket){
 
   console.log('user connected!');
 
-  if (io.sockets.connected[socket.id]) {
-      io.sockets.connected[socket.id].emit('msg', 'for your eyes only');
-  }
-
-  // Send the user the JSON object
-  io.emit('giveData', billyJSONObject);
-
-  // Get data from CLIENT
+  // Get data from CLIENT and send back data
   socket.on('giveUserComputerData', function(user_browser_user_agent){
     console.log(user_browser_user_agent + ' is here');
 
     io.to(socket.id).emit('msgs', 'You are ' + user_browser_user_agent);
-
-    //if (io.sockets.connected[socketid]) {
-    //io.sockets.connected[socketid].emit('message', 'for your eyes only');
-
-    // Send data to CLIENT
-    //io.emit('giveUserComputerData', user_browser_user_agent);
 
   });
 
